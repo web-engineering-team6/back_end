b70bdab74960b9ef1ba7ca3764659d1c4714766e
tazumeso <uribou800@gmail.com>
2017-07-07 17:44:08 +0900

    学習パート

--- deep_learnning/trainer.py
+++ deep_learnning/trainer.py
@@ -7,7 +7,7 @@ class Trainer:
 	def __init__(self, network):
 		self.network = network
 		
-	def train(self, optimizer, input_data, epoch_num, batch_size=100):
+	def train(self, optimizer, input_data, epoch_num, batch_size=100, save=None):
 		x_train = input_data["x_train"]
 		t_train = input_data["t_train"]
 		x_test = input_data["x_test"]
@@ -15,7 +15,7 @@ class Trainer:
 		train_loss_list = []
 		test_acc_list = []
 		train_num = len(x_train)
-		iter_num_per_epoch = train_num // batch_size
+		iter_num_per_epoch = train_num // batch_size  
 		for epoch in range(epoch_num):
 			mask_list = np.arange(train_num)
 			np.random.shuffle(mask_list)
@@ -37,8 +37,12 @@ class Trainer:
 			t_batch = t_test[batch_mask]
 			test_acc = self.network.accuracy(x_batch, t_batch)
 			test_acc_list.append(test_acc)
-			print('iter%i loss : %f' %(i, loss))
-			print('iter%i accuracy : %f' %(i, test_acc))  
+			print()   
+			print('epoch%i loss : %f' %(epoch + 1, loss))
+			print('epoch%i accuracy : %f' %(epoch + 1, test_acc)) 
+			if save is not None:
+				self.network.save_network(save + "/network_%i" % (epoch+1))
+			
 		
 		return train_loss_list, test_acc_list
 		
